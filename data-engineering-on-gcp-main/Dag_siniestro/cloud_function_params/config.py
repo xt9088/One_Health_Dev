08f09174c2012884bafa19a9b2a28d49716cc08d
xt9088 <xt9088@rimac.com.pe>
2024-06-26 09:16:43 -0500

    Versiones Finales 1.0 DAG y Cloud Function

--- data-engineering-on-gcp-main/Dag_siniestro/cloud_function_params/config.py
+++ data-engineering-on-gcp-main/Dag_siniestro/cloud_function_params/config.py
@@ -9,7 +9,8 @@ os.environ['DB_PASSWORD_SECRET_ID'] = 'csql_compass'
 os.environ['DB_NAME'] = 'db-compass'
 os.environ['DB_NAME'] = 'db-compass'
 os.environ['GCP_PROJECT'] = 'he-dev-data'
-
+os.environ['HOST'] = '35.245.223.109'
+os.environ['PORT'] = '3306'
 
 PREFIX = os.getenv('PREFIX', '')  # Set your prefix if needed
 WEB_SERVER_URL = os.getenv('WEB_SERVER_URL', 'https://80bb830408f34d3ca939b9b8d3524076-dot-us-east4.composer.googleusercontent.com')
