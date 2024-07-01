ffc6014c5b23ce69976b73decad5a6da6c0e033f
xt9088 <xt9088@rimac.com.pe>
2024-07-01 09:32:59 -0500

    Carpeta_Migracion_Validacion3_GCF_DAG_CSQL

--- data-engineering-on-gcp-main/Dag_siniestro/ddl_creacion.sql
+++ data-engineering-on-gcp-main/Dag_siniestro/ddl_creacion.sql
@@ -1,8 +1,8 @@
 SELECT t.ddl 
-FROM `he-dev-data.dev_data_analytics.INFORMATION_SCHEMA.TABLES` t
+FROM `he-dev-data.dev_data_landing.INFORMATION_SCHEMA.TABLES` t
 WHERE t.table_name = 'anl_tmp_part_month_siniestro_t';
 
-CREATE TABLE `he-dev-composer.dev_data_analytics.anl_tmp_part_month_siniestro_t`
+CREATE TABLE `he-dev-data.dev_data_analytics.anl_tmp_part_month_siniestro_t`
 (
   id_siniestro STRING,
   id_origen STRING,
